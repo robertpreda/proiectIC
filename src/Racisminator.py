fafe0661542e0c90aa804d6f3839d13ff92307a1
Sebastian Nagy <nagyseby98@gmail.com>
2020-03-09 16:44:33 +0200

    Integrated face detection with interface

--- src/Racisminator.py
+++ src/Racisminator.py
@@ -11,6 +11,8 @@ from src.model_backbones import get_model
 from tkinter import filedialog
 from threading import Thread
 from torchvision import transforms
+from src.detect_faces import get_boxes
+
 
 transform = transforms.Compose([
             transforms.ToPILImage(),
@@ -19,6 +21,10 @@ transform = transforms.Compose([
         ])
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 model = get_model("../models/squeezenet__1_1__4_classes_ epoch_3__1582729000.pth")
+
+face_cascade = 'face_classifier.xml'
+classifier = cv2.CascadeClassifier(face_cascade)
+race_info = [("#fffb6d", "Asian", "Corona"), ("#402D06", "Black", "Negro"), ("#c39752", "Latino", "Beaner"), ("#fef7d6", "White", "Gringo")]
 
 
 def run_model(cv_img):
@@ -36,13 +42,15 @@ class App:
         self.engine.setProperty('volume', 1)  # Volume 0-1
 
         self.window = window
+        self._callback_id = tkinter.StringVar(self.window)
+        self._callback_id.set(None)
+        self.window.protocol("WM_DELETE_WINDOW", self.window.destroy)
         self.window.iconphoto(True, PIL.ImageTk.PhotoImage(file="../Resources/Doofenshmirtz_Portrait.png"))
         self.window.geometry("1700x900+50+50")
         self.window.resizable(width=True, height=True)
         self.window.title(window_title)
         self.video_source = video_source
-        self.delay = 15
-        self.running = ""
+        self.delay = 10
         self.data = []
         self.dirname = "../Snapshots"
 
@@ -62,7 +70,6 @@ class App:
         self.pack_place_obj(self.canvas, x=250, y=50)
 
         self.graph_canvas = tkinter.Canvas(self.window, width=400, height=700)
-        self.race_info = [("#fffb6d", "Asian", "Corona"), ("#402D06", "Black", "Negro"), ("#c39752", "Latino", "Beaner"), ("#fef7d6", "White", "Gringo")]
 
         self.load_img_btn = tkinter.Button(self.window, text='Load Image', command=self.open_img)
         self.pack_place_obj(self.load_img_btn, height=25, width=100, x=632, y=15)
@@ -96,11 +103,11 @@ class App:
             # Top coordinates
             y = 400 - 400 * self.data[0][i][0][0]
             # Draw the bar
-            self.graph_canvas.create_rectangle(x0, 400, x1, y, fill=self.race_info[i][0])
+            self.graph_canvas.create_rectangle(x0, 400, x1, y, fill=race_info[i][0])
 
             # Put the y value above the bar
             self.graph_canvas.create_text(x0 + 2, y - 5, anchor=tkinter.SW, text=f"{round(self.data[0][i][0][0] * 100, 2)}%")
-            self.graph_canvas.create_text(x0 + 2, 415, anchor=tkinter.SW, text=self.race_info[i][1])
+            self.graph_canvas.create_text(x0 + 2, 415, anchor=tkinter.SW, text=race_info[i][1])
             x_gap = x_gap + 20
         self.show_graph()
 
@@ -124,28 +131,32 @@ class App:
         # Get a frame from the video source
         ret, frame = self.vid.get_frame()
         if ret:
-            self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
-            self.canvas.create_image(0, 0, image=self.photo, anchor=tkinter.NW)
-            results = run_model(frame)
-            self.data = F.softmax(results, dim=1).cpu().numpy()
-            self.draw_graph()
+            resized_cv_img = cv2.resize(frame, (1024, 768))
+            results, rgb_cv_image = get_resultimg_and_overallrace(resized_cv_img)
 
-        if self.running == "Video":
-            self.window.after(self.delay, self.update)
+            self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(rgb_cv_image))
+            self.canvas.create_image(0, 0, image=self.photo, anchor=tkinter.NW)
+
+            #results = run_model(frame)
+            #self.data = F.softmax(results, dim=1).cpu().numpy()
+
+            self.data = results
+            self.draw_graph()
+        self._callback_id.set(self.window.after(self.delay, self.update))
 
     def load_selected_image(self):
         try:
-            self.running = "Img"
+            self.window.after_cancel(self._callback_id.get())
             x = self.listbox.curselection()[0]
             self.open_img(f"{self.dirname}/{self.listbox.get(x)}")
         except IndexError:
             return
 
     def open_img(self, filename=""):
-        self.running = "Img"
+        self.window.after_cancel(self._callback_id.get())
         talk_thread = Thread(target=self.talk)
         ret, photo, results = self.img_obj.get_img(filename)
-        self.data = F.softmax(results, dim=1).cpu().numpy()
+        self.data = results
 
         if ret:
             self.canvas.delete("all")
@@ -160,7 +171,6 @@ class App:
         return
 
     def capture_video(self):
-        self.running = "Video"
         self.show_btn(self.btn_snapshot, height=25, width=100, x=712, y=830)
         self.show_btn(self.what_am_i_btn, height=25, width=100, x=600, y=830)
         self.canvas.delete("all")
@@ -172,7 +182,7 @@ class App:
     def talk(self):
         try:
             race = self.get_race()
-            self.engine.say(f"You are a {self.race_info[race][2]}!")
+            self.engine.say(f"You are a {race_info[race][2]}!")
             self.engine.runAndWait()
         except RuntimeError:
             return
@@ -257,14 +267,36 @@ class MyLoadImg:
         cv_img = cv2.imread(self.filename)
         resized_cv_img = cv2.resize(cv_img, (1024, 768))
 
-        tensor = run_model(cv_img)
-
-        rgb_cv_img = cv2.cvtColor(resized_cv_img, cv2.COLOR_BGR2RGB)
-
-        return True, rgb_cv_img, tensor
+        out_softed_tensor, rgb_cv_img = get_resultimg_and_overallrace(resized_cv_img)
+        return True, rgb_cv_img, out_softed_tensor
 
 
-# Create a window and pass it to the Application object
-MyApp = App(tkinter.Tk(), "Racisminator")
-cv2.destroyAllWindows()
+def get_resultimg_and_overallrace(resized_cv_img):
+    rects = get_boxes(resized_cv_img)
+    out_softed_tensor = [[[[0]], [[0]], [[0]], [[0]]]]
+    for x, y, w, h in rects:
+        aux_img = resized_cv_img[y:y + h, x:x + w]
+
+        # Uncomment if you want to see each face before processing
+        #cv2.imshow("image", aux_img)
+        #cv2.waitKey(0)
+        #cv2.destroyAllWindows()
+
+        tensor = run_model(aux_img)
+        tensor_soft = F.softmax(tensor, dim=1).cpu().numpy()
+        out_softed_tensor += tensor_soft
+        index = np.argmax(tensor_soft)
+
+        cv2.rectangle(resized_cv_img, (x, y), (x + w, y + h), color=(0, 255, 0),  thickness=1)  # Draw Rectangle with the coordinates
+        cv2.putText(resized_cv_img, race_info[index][1] + " " + f"{round(tensor_soft[0][index][0][0] * 100, 2)}", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), thickness=1)
+    rgb_cv_img = cv2.cvtColor(resized_cv_img, cv2.COLOR_BGR2RGB)
+    if len(rects) > 1:
+        out_softed_tensor = out_softed_tensor / len(rects)
+    return out_softed_tensor, rgb_cv_img
+
+
+if __name__ == "__main__":
+    # Create a window and pass it to the Application object
+    MyApp = App(tkinter.Tk(), "Racisminator")
+    cv2.destroyAllWindows()
 
--- src/detect_faces.py
+++ src/detect_faces.py
@@ -1,20 +1,33 @@
 import cv2
 
-face_cascade = 'face_classifier.xml'
-
+face_cascade = '../Resources/face_classifier.xml'
 classifier = cv2.CascadeClassifier(face_cascade)
 
-cap = cv2.VideoCapture(0)
 
-while True:
-    ret, orig_img = cap.read()
-    grey = cv2.cvtColor(orig_img, cv2.COLOR_BGR2GRAY)
-    rects =  classifier.detectMultiScale(grey, 1.1, 4)
-    for x,y,w,h in rects:
-        cv2.rectangle(orig_img, (x,y), (x+w, y+h), color=(255,0,0), thickness=2)
-    
-    cv2.imshow("Image", orig_img)
-    if cv2.waitKey(1) == 27:
-        cv2.destroyAllWindows()
-        cap.release()
-        exit()
+def get_boxes(image):
+    global classifier
+    grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    rects = classifier.detectMultiScale(grey, 1.1, 4)
+    return rects
+
+
+def main():
+    global classifier
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # This flag stops annoying warning
+    while True:
+        ret, orig_img = cap.read()
+        orig_img = cv2.resize(orig_img, (1024, 768))
+        grey = cv2.cvtColor(orig_img, cv2.COLOR_BGR2GRAY)
+        rects = classifier.detectMultiScale(grey, 1.1, 4)
+
+        for x, y, w, h in rects:
+            cv2.rectangle(orig_img, (x,y), (x+w, y+h), color=(255, 0, 0), thickness=2)
+        cv2.imshow("Image", orig_img)
+        if cv2.waitKey(1) == 27:
+            cv2.destroyAllWindows()
+            cap.release()
+            exit()
+
+
+if __name__ == "__main__":
+    main()
